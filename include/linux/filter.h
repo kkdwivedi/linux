9740ddd3430f9c5cde6984a5d9bee6f94e591cb7
Kumar Kartikeya Dwivedi <memxor@gmail.com>
2024-01-25 15:14:05 +0000

    bpf, x86: Implement runtime resource cleanup for exceptions
    Finally, tie all ends together and implement functionality to process a
    frame descriptor at runtime for each frame when bpf_throw is called, and
    release resources present on the program's stack frames.
    
    For each frame, we do bpf_cleanup_frame_resource, which will use the
    instruction pointer at runtime to figure out the right frame descriptor
    entry. After this, we explore all stack and registers and call their
    respective cleanup procedures.
    
    Next, if the frame corresponds to a subprog, we all save the location of
    where it has spilled its callers R6-R9 registers. If so, we record their
    value in the unwinding context. Only doing this when each frame has
    scratched the register in question allows us to arrive at the set of
    values actually needed during the freeing step for registers, regardless
    of how many callees existed and the varying locations of spilled callee
    saved registers. These registers can also lie across different frames,
    but will collected top down when arriving at a frame.
    
    Finally, after doing the cleanup, we go on to execute the exception
    callback and finish unwinding the stack.
    
    Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -963,6 +963,7 @@ bool bpf_jit_supports_subprog_tailcalls(void);
 bool bpf_jit_supports_kfunc_call(void);
 bool bpf_jit_supports_far_kfunc_call(void);
 bool bpf_jit_supports_exceptions(void);
+bool bpf_jit_supports_exceptions_cleanup(void);
 bool bpf_jit_supports_ptr_xchg(void);
 bool bpf_jit_supports_arena(void);
 void arch_bpf_stack_walk(bool (*consume_fn)(void *cookie, u64 ip, u64 sp, u64 bp), void *cookie);
@@ -1632,5 +1633,7 @@ static inline void bpf_xdp_copy_buf(struct xdp_buff *xdp, unsigned long off, voi
 {
 }
 #endif /* CONFIG_NET */
+
+void bpf_sk_release_dtor(void *ptr);
 
 #endif /* __LINUX_FILTER_H__ */
