a82e1a78e505aae58b285be4db7f3b88307b0772
Kumar Kartikeya Dwivedi <memxor@gmail.com>
2026-08-10 02:30:51 +0200

    selftests/bpf: Cover typed arena declaration collection
    Exercise collection of multiple ordinary arena types and a type containing a
    supported spin lock field. Also verify that zero-sized objects and special
    fields whose runtime behavior is not implemented yet are rejected.
    
    Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>

--- tools/testing/selftests/bpf/verifier/arena_type_cast.c
+++ tools/testing/selftests/bpf/verifier/arena_type_cast.c
@@ -1,5 +1,5 @@
 #define ARENA_CAST_BTF_TYPES \
-	.btf_strings = "\0int\0value\0arena_obj\0other_obj\0", \
+	.btf_strings = "\0int\0value\0arena_obj\0other_obj\0empty_obj\0", \
 	.btf_types = { \
 	/* 1: int */ \
 	BTF_TYPE_INT_ENC(1, BTF_INT_SIGNED, 0, 32, 4), \
@@ -9,6 +9,30 @@
 	/* 3: struct other_obj { int value; } */ \
 	BTF_STRUCT_ENC(21, 1, 4), \
 	BTF_MEMBER_ENC(5, 1, 0), \
+	/* 4: struct empty_obj {}; */ \
+	BTF_STRUCT_ENC(31, 0, 0), \
+	BTF_END_RAW \
+	}
+
+#define ARENA_SPIN_LOCK_BTF_TYPES \
+	.btf_strings = "\0bpf_spin_lock\0lock_obj\0lock\0", \
+	.btf_types = { \
+	/* 1: struct bpf_spin_lock */ \
+	BTF_STRUCT_ENC(1, 0, 4), \
+	/* 2: struct lock_obj { struct bpf_spin_lock lock; } */ \
+	BTF_STRUCT_ENC(15, 1, 4), \
+	BTF_MEMBER_ENC(24, 1, 0), \
+	BTF_END_RAW \
+	}
+
+#define ARENA_TIMER_BTF_TYPES \
+	.btf_strings = "\0bpf_timer\0timer_obj\0timer\0", \
+	.btf_types = { \
+	/* 1: struct bpf_timer */ \
+	BTF_STRUCT_ENC(1, 0, sizeof(struct bpf_timer)), \
+	/* 2: struct timer_obj { struct bpf_timer timer; } */ \
+	BTF_STRUCT_ENC(11, 1, sizeof(struct bpf_timer)), \
+	BTF_MEMBER_ENC(21, 1, 0), \
 	BTF_END_RAW \
 	}
 
@@ -34,8 +58,58 @@
 	.unexpected_insns = { ARENA_TYPE_CAST_INSN(BPF_REG_0, 2) },
 	.fixup_map_arena = { 1 },
 	.prog_type = BPF_PROG_TYPE_SOCKET_FILTER,
-	.result = ACCEPT,
+	.result = VERBOSE_ACCEPT,
+	.errstr = "arena type ID 2: size=4 slot_size=4\t"
+		  "arena type ID 3: size=4 slot_size=4",
 	ARENA_CAST_BTF_TYPES
+},
+{
+	"arena type cast: collect supported special fields",
+	.insns = {
+	BPF_MOV64_IMM(BPF_REG_0, 0),
+	BPF_LD_MAP_FD(BPF_REG_1, 0),
+	ADDR_SPACE_CAST_INSN(BPF_REG_0),
+	ARENA_TYPE_CAST_INSN(BPF_REG_0, 2),
+	BPF_MOV64_IMM(BPF_REG_0, 0),
+	BPF_EXIT_INSN(),
+	},
+	.fixup_map_arena = { 1 },
+	.prog_type = BPF_PROG_TYPE_SOCKET_FILTER,
+	.result = VERBOSE_ACCEPT,
+	.errstr = "arena type ID 2: size=4 slot_size=4",
+	ARENA_SPIN_LOCK_BTF_TYPES
+},
+{
+	"arena type cast: reject zero-sized declaration",
+	.insns = {
+	BPF_MOV64_IMM(BPF_REG_0, 0),
+	BPF_LD_MAP_FD(BPF_REG_1, 0),
+	ADDR_SPACE_CAST_INSN(BPF_REG_0),
+	ARENA_TYPE_CAST_INSN(BPF_REG_0, 4),
+	BPF_MOV64_IMM(BPF_REG_0, 0),
+	BPF_EXIT_INSN(),
+	},
+	.fixup_map_arena = { 1 },
+	.prog_type = BPF_PROG_TYPE_SOCKET_FILTER,
+	.result = REJECT,
+	.errstr = "arena type ID 4 has zero size",
+	ARENA_CAST_BTF_TYPES
+},
+{
+	"arena type cast: reject unsupported special fields",
+	.insns = {
+	BPF_MOV64_IMM(BPF_REG_0, 0),
+	BPF_LD_MAP_FD(BPF_REG_1, 0),
+	ADDR_SPACE_CAST_INSN(BPF_REG_0),
+	ARENA_TYPE_CAST_INSN(BPF_REG_0, 2),
+	BPF_MOV64_IMM(BPF_REG_0, 0),
+	BPF_EXIT_INSN(),
+	},
+	.fixup_map_arena = { 1 },
+	.prog_type = BPF_PROG_TYPE_SOCKET_FILTER,
+	.result = REJECT,
+	.errstr = "arena type ID 2 field bpf_timer is not supported",
+	ARENA_TIMER_BTF_TYPES
 },
 {
 	"arena type cast: reject scalar input",
@@ -128,4 +202,6 @@
 
 #undef ARENA_TYPE_CAST_INSN
 #undef ADDR_SPACE_CAST_INSN
+#undef ARENA_TIMER_BTF_TYPES
+#undef ARENA_SPIN_LOCK_BTF_TYPES
 #undef ARENA_CAST_BTF_TYPES
